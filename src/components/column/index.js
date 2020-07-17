6980956d511d091f2b46ff0e140f00791bb8bd9d
LJ Spencer <hockeyduck3@gmail.com>
2020-07-17 14:58:44 -0600

    Added components for Row and Column

---
+++ src/components/column/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Column(props) {
+    return <div className="col-lg-12">{props.children}</div>
+}
+
+export default Column;
--- src/components/main-content/index.js
+++ src/components/main-content/index.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 
 import API from '../../util/api';
 
+import Row from '../row/index';
+import Column from '../column/index';
+
 class Main extends Component {
     state = {
         results: []
@@ -12,12 +15,26 @@ class Main extends Component {
             this.setState({
                 results: res.data.results
             });
+
+            console.log(this.state.results);
         });
     }
 
     render() {
+        const results = this.state.results;
+
         return (
-            <h1>Hello world</h1>
+            <div>
+                {
+                    results.map((employees, i) => (
+                        <Row key={i}>
+                            <Column>
+                                <p>{employees.name.first}</p>
+                            </Column>
+                        </Row>
+                    ))
+                }
+            </div>
         )
     }
 }
